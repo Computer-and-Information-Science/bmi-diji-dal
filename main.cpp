b348337dbcf76e8375ac8d2e3fdca08b866d08ff
diji-dal <samuel.kroll@student.northampton.edu>
2026-01-28 18:55:27 +0000

    Wrote BMI program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
- 
+    //imperial height and weight
+    const int height = 61;
+    const int weight = 130;
 
+    //converts weight and height to metric units
+    float kgs = weight * 0.453592;
+    float meters = height * 0.0254;
 
+    float BMI = kgs / (meters * meters);
+
+    cout << "The BMI for this person is: " << BMI << endl;
+
+    return BMI;
 }
